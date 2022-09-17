8b56e6532a3bade58351a348ec8660e5ef2a4c91
Anton Boev <110462969+t0H1k@users.noreply.github.com>
2022-09-17 11:41:40 +0300

    Initial Commit

---
+++ trafficLightApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trafficLightApp
+//
+//  Created by Anton Boev on 17.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
