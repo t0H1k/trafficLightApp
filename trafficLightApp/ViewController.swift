2f2858d917bd4ef2db6a6e60c8f2184452dc2bf6
Anton Boev <110462969+t0H1k@users.noreply.github.com>
2022-09-17 16:27:09 +0300

    implementation of the installation of constraints on the traffic light and the button implementation of rounding the corners of the traffic light implementation of the operation of the traffic light by pressing the button

--- trafficLightApp/ViewController.swift
+++ trafficLightApp/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redTrafficLight: UIView!
+    @IBOutlet var yellowTrafficLight: UIView!
+    @IBOutlet var greenTrafficLight: UIView!
+    @IBOutlet var trafficLightButton: UIButton!
+    
+    override func viewDidLayoutSubviews() {
+        redTrafficLight.layer.cornerRadius = redTrafficLight.frame.height / 2
+        yellowTrafficLight.layer.cornerRadius = yellowTrafficLight.frame.height / 2
+        greenTrafficLight.layer.cornerRadius =  greenTrafficLight.frame.height / 2
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        trafficLightButton.layer.cornerRadius = 10
+        redTrafficLight.alpha = 0.3
+        yellowTrafficLight.alpha = 0.3
+        greenTrafficLight.alpha = 0.3
     }
-
-
+    
+    @IBAction func trafficLightButtonDidTapped() {
+        if trafficLightButton.titleLabel!.text == "START" {
+            redTrafficLight.alpha = 1
+            trafficLightButton.setTitle("NEXT", for: .normal)
+        } else if redTrafficLight.alpha == 1 {
+            redTrafficLight.alpha = 0.3
+            yellowTrafficLight.alpha = 1
+        } else if yellowTrafficLight.alpha == 1 {
+            yellowTrafficLight.alpha = 0.3
+            greenTrafficLight.alpha = 1
+        } else {
+            greenTrafficLight.alpha = 0.3
+            redTrafficLight.alpha = 1
+        }
+    }
 }
+
 
